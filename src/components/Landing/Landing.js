9377c7296b60061708e1de242d9a9b61e128e644
Sean White <sean.b.white89@icloud.com>
2018-08-19 16:46:34 -0600

    finish

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,7 +6,7 @@ import Particles from 'react-particles-js'
 export default class Landing extends Component {
   login(){
     let {REACT_APP_DOMAIN,REACT_APP_CLIENT_ID} = process.env
-    let url = encodeURIComponent(`http://localhost:3099/auth/callback`)
+    let url = `${window.location.origin}/auth/callback`
     window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`
   }
   render(){
